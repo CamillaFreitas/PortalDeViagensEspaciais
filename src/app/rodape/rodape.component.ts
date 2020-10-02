f09f803546670358cb6d48056f666c49890e0d2a
Camilla Freitas <camirsfreitas@gmail.com>
2020-10-02 17:34:43 -0300

    Rodape finalizado

--- src/app/rodape/rodape.component.ts
+++ src/app/rodape/rodape.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rodape',
   templateUrl: './rodape.component.html',
   styleUrls: ['./rodape.component.css']
 })
-export class RodapeComponent implements OnInit {
+export class RodapeComponent {
 
-  constructor() { }
+    obterClasseTitulo(){
+      return ['titulo'];
+    }
 
-  ngOnInit(): void {
-  }
-
+    obterClasseContainerRodape(){
+      return ['container-rodape'];
+    }
 }
